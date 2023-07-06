a9314c259c90c831a2db48328db8dc8ac9a1bd42
AlexLojko <a_l_g@tut.by>
2023-07-06 20:45:57 +0300

    add s4

--- src/main/java/org/example/Seminar4.java
+++ src/main/java/org/example/Seminar4.java
@@ -7,53 +7,62 @@ import java.util.Scanner;
 public class Seminar4 {
     static Scanner scanner;
 
-    static public void sort1(ArrayList<String> ma, ArrayList<String> ms, ArrayList<String> md, ArrayList<Integer> mf, ArrayList<Boolean> mz, ArrayList<Integer> id) {
+    static public void sortage(ArrayList<String> name, ArrayList<String> lastname, ArrayList<String> surname, ArrayList<Integer> age, ArrayList<Boolean> gender, ArrayList<Integer> id) {
         id.sort(new Comparator<Integer>() {
             @Override
             public int compare(Integer o1, Integer o2) {
 
-                return mf.get(o1) - mf.get(o2);
+                return age.get(o1) - age.get(o2);
             }
 
         });
         for (int i = 0; i < id.size(); i++) {
-            System.out.println(ma.get(id.get(i)) + " " + ms.get(id.get(i)).toUpperCase().charAt(0) + "." + md.get(id.get(i)).toUpperCase().charAt(0) + "." + " " + mf.get(id.get(i)) + " " + (mz.get(id.get(i)) ? "Ж" : "М"));
+            System.out.println(name.get(id.get(i)) + " " + lastname.get(id.get(i)).toUpperCase().charAt(0) + "." + surname.get(id.get(i)).toUpperCase().charAt(0) + "." + " " + age.get(id.get(i)) + " " + (gender.get(id.get(i)) ? "Ж" : "М"));
         }
     }
 
-
-    static public void sort2(ArrayList<String> ma, ArrayList<String> ms, ArrayList<String> md, ArrayList<Integer> mf, ArrayList<Boolean> mz, ArrayList<Integer> id) {
+    static public void sortgenderage(ArrayList<String> name, ArrayList<String> lastname, ArrayList<String> surname, ArrayList<Integer> age, ArrayList<Boolean> gender, ArrayList<Integer> id) {
         id.sort(new Comparator<Integer>() {
             @Override
             public int compare(Integer o1, Integer o2) {
-                if (mz.get(o1) == mz.get(o2)) return mf.get(o1) - mf.get(o2);
+                if (gender.get(o1)==true) return 1;
+                else return -1;
+            }
+
+        });
+        id.sort(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                if (gender.get(o1) == gender.get(o2)) return age.get(o1) - age.get(o2);
                 return 0;
             }
         });
         for (int i = 0; i < id.size(); i++) {
-            System.out.println(ma.get(id.get(i)) + " " + ms.get(id.get(i)).toUpperCase().charAt(0) + "." + md.get(id.get(i)).toUpperCase().charAt(0) + "." + " " + mf.get(id.get(i)) + " " + (mz.get(id.get(i)) ? "Ж" : "М"));
+            System.out.println(name.get(id.get(i)) + " " + lastname.get(id.get(i)).toUpperCase().charAt(0) + "." + surname.get(id.get(i)).toUpperCase().charAt(0) + "." + " " + age.get(id.get(i)) + " " + (gender.get(id.get(i)) ? "Ж" : "М"));
         }
     }
+
+
     public static void main(String[] args) {
         scanner = new Scanner(System.in);
-        ArrayList<String> ma = new ArrayList<>();
-        ArrayList<String> ms = new ArrayList<>();
-        ArrayList<String> md = new ArrayList<>();
-        ArrayList<Integer> mf = new ArrayList<>();
-        ArrayList<Boolean> mz = new ArrayList<>();
+        ArrayList<String> name = new ArrayList<>();
+        ArrayList<String> lastname = new ArrayList<>();
+        ArrayList<String> surname = new ArrayList<>();
+        ArrayList<Integer> age = new ArrayList<>();
+        ArrayList<Boolean> gender = new ArrayList<>();
         ArrayList<Integer> id = new ArrayList<>();
         while (true) {
             System.out.println("Введите фамилию -> ");
-            ma.add(scanner.nextLine());
+            name.add(scanner.nextLine());
             System.out.println("Введите имя -> ");
-            ms.add(scanner.nextLine());
+            lastname.add(scanner.nextLine());
             System.out.println("Введите отчество -> ");
-            md.add(scanner.nextLine());
+            surname.add(scanner.nextLine());
             System.out.println("Введите возраст -> ");
-            mf.add(Integer.valueOf(scanner.nextLine()));
+            age.add(Integer.valueOf(scanner.nextLine()));
             System.out.println("Введите пол -> ");
-            mz.add(scanner.nextLine().toLowerCase().contains("ж"));
-            id.add(mz.size() - 1);
+            gender.add(scanner.nextLine().toLowerCase().contains("ж"));
+            id.add(gender.size() - 1);
             System.out.println("Продолжить ввод да/нет? -> ");
             String tmp = scanner.nextLine();
             if (tmp.toLowerCase().contains("нет")) break;
@@ -64,16 +73,16 @@ public class Seminar4 {
 
 
 
-        for (int i = 0; i < ma.size(); i++) {
-            System.out.println(ma.get(i) + " " + ms.get(i).toUpperCase().charAt(0) + "." + md.get(i).toUpperCase().charAt(0) + "." + " " + mf.get(i) + " " + (mz.get(i) ? "Ж" : "М"));
+        for (int i = 0; i < name.size(); i++) {
+            System.out.println(name.get(i) + " " + lastname.get(i).toUpperCase().charAt(0) + "." + surname.get(i).toUpperCase().charAt(0) + "." + " " + age.get(i) + " " + (gender.get(i) ? "Ж" : "М"));
         }
 
-//        System.out.println("Сортировать по возрасту да/нет? -> ");
-//        String tmp = scanner.nextLine();
-//        if (tmp.toLowerCase().contains("да")) sort1(ma, ms, md, mf, mz, id);
+        System.out.println("Сортировать по возрасту да/нет? -> ");
+        String tmp = scanner.nextLine();
+        if (tmp.toLowerCase().contains("да")) sortage(name, lastname, surname, age, gender, id);
 
         System.out.println("Сортировать по возрасту и полу да/нет? -> ");
         String tmp1 = scanner.nextLine();
-        if (tmp1.toLowerCase().contains("да")) sort2(ma, ms, md, mf, mz, id);
+        if (tmp1.toLowerCase().contains("да")) sortgenderage(name, lastname, surname, age, gender, id);
     }
 }
