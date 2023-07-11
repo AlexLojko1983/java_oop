8beb2591c4084c6a2d92444908b288098a6af99a
AlexLojko <a_l_g@tut.by>
2023-07-11 20:26:57 +0300

    add s4

--- src/main/java/org/example/Dz_5.java
+++ src/main/java/org/example/Dz_5.java
@@ -1,44 +1,48 @@
 package org.example;
 
 import java.io.IOException;
+import java.security.Key;
 import java.util.*;
+import java.util.function.Function;
+
+
+//Задание
+//        Реализуйте структуру телефонной книги с помощью HashMap.
+//        Программа также должна учитывать, что во входной структуре будут повторяющиеся имена с разными телефонами,
+//        их необходимо считать, как одного человека с разными телефонами. Вывод должен быть отсортирован по убыванию числа телефонов.
 
 public class Dz_5 {
-    static class DupliMap extends HashMap<String, List<String>> {
-        DupliMap(String... objects) throws IOException {
-            super();
-
-            if (objects.length % 2 != 0) throw new IOException("Failure odd number of parameters to be entered");
-
-            for (int i = 0; i < objects.length; i += 2) this.ponebook(objects[i], objects[i + 1]);
-        }
-
-        DupliMap ponebook(String key, String value) {
-            this.computeIfAbsent(key, k -> new ArrayList<>()).add(value);
-
-            return this;
-        }
-        static class addvalue {
-
-        }
-
-        void print() { System.out.println(this); }
+    public static HashMap book(HashMap<String, List<String>> ponebook, String key, String value) {
+        ponebook.computeIfAbsent(key, k -> new ArrayList<>()).add(value);
+        return ponebook;
     }
 
-
-
-
     public static void main(String[] args) throws IOException {
-        HashMap<String, String> ponebook = new HashMap<>();
+        HashMap<String, List<String>> ponebook = new HashMap<>();
+
+        ponebook = book(ponebook,"Иванов","4897434");
+        ponebook = book(ponebook,"Иванов","68786");
+        ponebook = book(ponebook,"Сидоров","4648848");
+        ponebook = book(ponebook,"Сидоров","464558848");
+        ponebook = book(ponebook,"Сидоров","5");
+
+//        st.sorted(Comparator.comparing(e -> e.getValue()))
+//                .forEach(e ->result.put(e.getKey(),e.getValue()));
+
+//        ponebook.entrySet()
+//                .stream()
+//                .sorted(Map.Entry.comparingByKey())
+//                .forEach(System.out::println);
+
+        ponebook.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue((o1, o2) -> o2.size()-o1.size()))
+                .forEach(System.out::println);
 
 
-        ponebook.put("Иванов", String.valueOf("4621876"));
-        ponebook.put("Иванов", String.valueOf("4897434"));
-
-        new  DupliMap("Иванов", "4621876","Иванов", "4897434").print();
-
-//        for(HashMap.Entry<String,String> k: ponebook.entrySet()) {
-//            System.out.println(k.getValue() + ": " + k.getKey());
+//        for(HashMap.Entry<String,List<String>> k: ponebook.entrySet()) {
+//
+//            System.out.println(k.getKey() + ": " + k.getValue());
 //        }
     }
 }
