ea03691edd6813a78682b9391d03cee887bde48f
AlexLojko <a_l_g@tut.by>
2023-07-12 22:29:29 +0300

    add s6

--- src/main/java/org/example/Seminar6.java
+++ src/main/java/org/example/Seminar6.java
@@ -2,10 +2,7 @@ package org.example;
 
 
 import javax.swing.text.html.HTMLDocument;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 
 public class Seminar6 {
@@ -15,44 +12,58 @@ public class Seminar6 {
 
         System.out.println((myset.add(4165)));
         System.out.println((myset.add(4155565)));
-        System.out.println((myset.add(null)));
+        System.out.println((myset.add(0)));
         System.out.println((myset.add(4165)));
+        System.out.println((myset.add(0)));
         System.out.println(myset);
 //        System.out.println((myset.remove(4165)));
 //        System.out.println(myset.remove(null));
 //        System.out.println(myset.remove(65456));
         System.out.println(myset);
         Iterator<Integer> iter = myset.iterator();
-        while (iter.hasNext()){
-            System.out.printf("%d ",iter.next());
+        while (iter.hasNext()) {
+            System.out.printf("%d ", iter.next());
         }
+        System.out.println();
+        System.out.println(myset.get(0));
+        System.out.println(myset.get(1));
+        System.out.println(myset.get(10));
 
 
     }
 }
+
 class MySet<T> {
     private HashMap<T, Object> map = new HashMap<>();
     private static final Object myo = new Object();
 
 
     public boolean add(T value) {
-        return map.put(value,myo) == null;
+        return map.put(value, myo) == null;
     }
-    public boolean remove(T value){
+
+    public boolean remove(T value) {
         return map.remove(value) == myo;
     }
-    public String toString(){
+
+    public String toString() {
         return map.keySet().toString();
     }
-    public Iterator<T> iterator(){
+
+    public Iterator<T> iterator() {
         return map.keySet().iterator();
     }
-    public void key() {
-        Set<Map.Entry<T, Object>> entrySet = map.entrySet();
-        for (Map.Entry<T, Object> pair : entrySet) {
-            if (myo.equals(pair.getValue())) {
-                System.out.println(pair.getKey());// нашли наше значение и возвращаем  ключ
-            }
+
+    public Object tos() {
+        return map.values().toString();
+    }
+
+    public T get(Integer i) {
+        ArrayList<T> setmy = new ArrayList<>();
+        for (T item : map.keySet()) {
+            setmy.add(item);
         }
+        if (setmy.size() >= i) return setmy.get(i);
+        return null;
     }
 }
